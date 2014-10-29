dcfc452a9e1dd713a26b8f19cc0a126292049a7f
libertyernie <maybeway36@gmail.com>
2014-10-28 21:40:19 -0500

    quick & dirty settings storage

---
+++ WeasylSync/Settings.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeasylSync {
+	public class Settings {
+		public string AccessTokenKey { get; set; }
+		public string AccessTokenSecretEnc { get; set; }
+
+		public static Settings Global;
+
+		static Settings() {
+			if (File.Exists("WeasylSyncSettings.json")) {
+				Global = JsonConvert.DeserializeObject<Settings>(File.ReadAllText("WeasylSyncSettings.json"));
+			} else {
+				Global = new Settings();
+			}
+		}
+
+		public void Save(string filename = "WeasylSyncSettings.json") {
+			File.WriteAllText(filename, JsonConvert.SerializeObject(Global));
+		}
+	}
+}
