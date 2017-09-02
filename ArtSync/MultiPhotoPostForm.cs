a2857e826baa5ff81e92aebac9a3de47c43bc127
libertyernie <maybeway36@gmail.com>
2017-09-02 13:46:18 -0500

    Add images in order of selection

--- ArtSync/MultiPhotoPostForm.cs
+++ ArtSync/MultiPhotoPostForm.cs
@@ -15,12 +15,16 @@ using System.Windows.Forms;
 namespace ArtSync {
     public partial class MultiPhotoPostForm : Form {
         private ISiteWrapper _wrapper;
-        private ISubmissionWrapper[] _submissions;
+
+        private List<int> _selectedIds;
+        
         private PictureBox[] _picBoxes;
 
         private IEnumerable<ISubmissionWrapper> GetSelectedSubmissions() {
-            foreach (ISubmissionWrapper submission in listBox1.SelectedItems) {
-                yield return submission;
+            foreach (int index in _selectedIds) {
+                if (listBox1.Items[index] is ISubmissionWrapper submission) {
+                    yield return submission;
+                }
             }
         }
 
@@ -30,13 +34,15 @@ namespace ArtSync {
             _wrapper = wrapper;
             RepopulateList();
 
+            _selectedIds = new List<int>();
+
             _picBoxes = new[] { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
-            _submissions = new ISubmissionWrapper[_picBoxes.Length];
             
             for (int i = 0; i < _picBoxes.Length; i++) {
                 int j = i;
                 _picBoxes[j].Click += (o, e) => {
-                    string url = _submissions[j]?.ViewURL;
+                    ISubmissionWrapper submission = listBox1.Items[j] as ISubmissionWrapper;
+                    string url = submission?.ViewURL;
                     if (url != null) Process.Start(url);
                 };
             }
@@ -51,14 +57,19 @@ namespace ArtSync {
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            foreach (int index in listBox1.SelectedIndices) {
+                if (!_selectedIds.Contains(index)) _selectedIds.Add(index);
+            }
+            foreach (int index in _selectedIds.ToList()) {
+                if (!listBox1.SelectedIndices.Contains(index)) _selectedIds.Remove(index);
+            }
+
             var selected = GetSelectedSubmissions().Take(4).ToList();
             for (int i = 0; i < _picBoxes.Length; i++) {
                 if (i < selected.Count) {
-                    _submissions[i] = selected[i];
                     _picBoxes[i].ImageLocation = selected[i].ThumbnailURL;
                     _picBoxes[i].Cursor = Cursors.Hand;
                 } else {
-                    _submissions[i] = null;
                     _picBoxes[i].ImageLocation = null;
                     _picBoxes[i].Cursor = Cursors.Default;
                 }
