ce16522d9017c666446aa2336c1a682d30097710
libertyernie <maybeway36@gmail.com>
2018-03-06 09:13:46 -0600

    Don't use IE cookies

--- CrosspostSharp3/Program.cs
+++ CrosspostSharp3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ISchemm.WinFormsOAuth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace CrosspostSharp3 {
 		static void Main() {
 			ExceptionHandler.SwallowWebExceptions = false;
 			TweetinviConfig.CurrentThreadSettings.TweetMode = TweetMode.Extended;
+
+			IECookiePersist.Suppress(true);
+			IECompatibility.SetForCurrentProcess();
 
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
