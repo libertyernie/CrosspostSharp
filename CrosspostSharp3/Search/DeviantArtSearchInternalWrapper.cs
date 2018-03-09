9651cf148efa61ed40a494897f1b705df4209273
libertyernie <maybeway36@gmail.com>
2018-03-09 17:38:57 -0600

    Create a new wrapper for DeviantArt searching

---
+++ CrosspostSharp3/Search/DeviantArtSearchInternalWrapper.cs
@@ -0,0 +1,38 @@
+﻿using ArtSourceWrapper;
+using DeviantartApi.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrosspostSharp3.Search {
+	internal class DeviantArtSearchInternalWrapper : AsynchronousCachedEnumerable<Deviation, uint> {
+		public override int BatchSize { get; set; } = 24;
+		public override int MinBatchSize => 1;
+		public override int MaxBatchSize => 24;
+
+		protected override async Task<InternalFetchResult> InternalFetchAsync(uint? startPosition, int count) {
+			uint maxCount = (uint)Math.Max(MinBatchSize, Math.Min(MaxBatchSize, count));
+			uint position = startPosition ?? 0;
+
+			var galleryResponse = await new DeviantartApi.Requests.Browse.NewestRequest() {
+				Query = "star trek",
+				Limit = maxCount,
+				Offset = startPosition
+			}.GetNextPageAsync();
+
+			if (galleryResponse.IsError) {
+				throw new DeviantArtException(galleryResponse.ErrorText);
+			}
+			if (!string.IsNullOrEmpty(galleryResponse.Result.Error)) {
+				throw new DeviantArtException(galleryResponse.Result.ErrorDescription);
+			}
+
+			return new InternalFetchResult(
+				galleryResponse.Result.Results,
+				position + (uint)galleryResponse.Result.Results.Count,
+				!galleryResponse.Result.HasMore);
+		}
+	}
+}
