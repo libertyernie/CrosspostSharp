5ab48d87b9229eb72714a8bc0571ef5b5fcc1f5d
libertyernie <maybeway36@gmail.com>
2018-12-07 21:11:18 -0600

    Try to remove more blank lines in HTML->text conversion

--- CrosspostSharp3/HtmlConversion.cs
+++ CrosspostSharp3/HtmlConversion.cs
@@ -10,6 +10,7 @@ namespace CrosspostSharp3 {
 	public static class HtmlConversion {
 		public static string ConvertHtmlToText(string html) {
 			html = html.Replace("<div><br></div>", "");
+			html = html.Replace("<br></div>", "</div>");
 			using (var w = new System.Windows.Forms.WebBrowser()) {
 				w.Navigate("about:blank");
 				w.Document.Write(html);
