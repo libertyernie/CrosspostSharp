dcfd587663bf0eac1b6e02c451d93087a61be049
libertyernie <maybeway36@gmail.com>
2018-12-14 15:53:22 -0600

    Start adding new library for dA

---
+++ DeviantArtLib.WinForms/DeviantArtAuthorizationCodeForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeviantArtLib.WinForms {
+	public class DeviantArtAuthorizationCodeForm : Form {
+		public string Code { get; private set; }
+
+		public DeviantArtAuthorizationCodeForm(string clientId, Uri callbackUrl, IEnumerable<string> scopes = null) {
+			this.Width = 322;
+			this.Height = 573;
+
+			var webBrowser1 = new WebBrowser {
+				Dock = DockStyle.Fill,
+				ScriptErrorsSuppressed = true
+			};
+			this.Controls.Add(webBrowser1);
+
+			this.Shown += (o, e) => {
+				StringBuilder sb = new StringBuilder();
+				sb.Append($"response_type=code&");
+				sb.Append($"client_id={clientId}&");
+				sb.Append($"redirect_uri={callbackUrl}");
+				if (scopes != null) {
+					sb.Append($"&scope={WebUtility.UrlEncode(string.Join(" ", scopes))}");
+				}
+				webBrowser1.Navigate("https://www.deviantart.com/oauth2/authorize?" + sb);
+			};
+
+			webBrowser1.Navigated += (o, e) => {
+				if (e.Url.Authority == callbackUrl.Authority && e.Url.AbsolutePath == callbackUrl.AbsolutePath) {
+					int codeIndex = e.Url.Query.IndexOf("code=");
+					if (codeIndex > -1) {
+						string code = e.Url.Query.Substring(codeIndex + 5);
+						if (code.Contains("&")) code = code.Substring(0, code.IndexOf("&"));
+						Code = code;
+						DialogResult = DialogResult.OK;
+					}
+				}
+			};
+
+			webBrowser1.DocumentTitleChanged += (o, e) => {
+				this.Text = webBrowser1.DocumentTitle;
+			};
+
+			webBrowser1.Navigating += (o, e) => {
+				if (e.Url.OriginalString.StartsWith("javascript:void")) e.Cancel = true;
+			};
+		}
+	}
+}
---
+++ DeviantArtLib.WinForms/DeviantArtImplicitGrantForm.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeviantArtLib.WinForms {
+	public class DeviantArtImplicitGrantForm : Form {
+		private readonly string _state;
+
+		public string AccessToken { get; private set; }
+		public DateTimeOffset? ExpiresAt { get; private set; }
+
+		public DeviantArtImplicitGrantForm(string clientId, Uri callbackUrl, IEnumerable<string> scopes = null) {
+			_state = Guid.NewGuid().ToString();
+
+			this.Width = 322;
+			this.Height = 573;
+
+			var webBrowser1 = new WebBrowser {
+				Dock = DockStyle.Fill,
+				ScriptErrorsSuppressed = true
+			};
+			this.Controls.Add(webBrowser1);
+
+			this.Shown += (o, e) => {
+				StringBuilder sb = new StringBuilder();
+				sb.Append($"response_type=token&");
+				sb.Append($"state={WebUtility.UrlEncode(_state)}&");
+				sb.Append($"client_id={clientId}&");
+				sb.Append($"redirect_uri={callbackUrl}");
+				if (scopes != null) {
+					sb.Append($"&scope={WebUtility.UrlEncode(string.Join(" ", scopes))}");
+				}
+				webBrowser1.Navigate("https://www.deviantart.com/oauth2/authorize?" + sb);
+			};
+
+			webBrowser1.Navigated += (o, e) => {
+				if (e.Url.Authority == callbackUrl.Authority && e.Url.AbsolutePath == callbackUrl.AbsolutePath) {
+					var psd = QueryHelpers.ParseQuery(e.Url.Fragment.Substring(1));
+					if (!psd.TryGetValue("access_token", out StringValues access_token)) return;
+					if (!psd.TryGetValue("token_type", out StringValues token_type)) return;
+					if (!psd.TryGetValue("state", out StringValues state)) return;
+					if (state == _state && token_type == "bearer") {
+						AccessToken = access_token;
+						if (psd.TryGetValue("expires_in", out StringValues expires_in)) {
+							if (double.TryParse(expires_in, out double expsec)) {
+								ExpiresAt = DateTimeOffset.Now.AddSeconds(expsec);
+							}
+						}
+						webBrowser1.Navigate("about:blank");
+					}
+				} else if (e.Url.AbsoluteUri == "about:blank") {
+					DialogResult = DialogResult.OK;
+				}
+			};
+
+			webBrowser1.NewWindow += (o, e) => {
+
+			};
+
+			webBrowser1.DocumentTitleChanged += (o, e) => {
+				this.Text = webBrowser1.DocumentTitle;
+			};
+
+			webBrowser1.Navigating += (o, e) => {
+				if (e.Url.OriginalString.StartsWith("javascript:void")) e.Cancel = true;
+			};
+		}
+	}
+}
