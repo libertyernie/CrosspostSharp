5f46c1584b939fd5db2f66cfb6698a7d4964b6c7
libertyernie <maybeway36@gmail.com>
2018-12-21 12:29:10 -0600

    Add sta.sh delete

---
+++ DeviantArtFs/Stash/Delete.fs
@@ -0,0 +1,24 @@
+﻿namespace DeviantArtFs.Stash
+
+open DeviantArtFs
+
+module Delete =
+    open System.IO
+    open System.Threading.Tasks
+
+    let AsyncExecute token (itemid: int64) = async {
+        let req = dafs.createRequest token "https://www.deviantart.com/api/v1/oauth2/stash/delete"
+        req.Method <- "POST"
+        req.ContentType <- "application/x-www-form-urlencoded"
+
+        do! async {
+            use! stream = req.GetRequestStreamAsync() |> Async.AwaitTask
+            use sw = new StreamWriter(stream)
+            do! sprintf "itemid=%d" itemid |> sw.WriteAsync |> Async.AwaitTask
+        }
+
+        let! json = dafs.asyncRead req
+        return ignore json
+    }
+
+    let ExecuteAsync token itemid = AsyncExecute token itemid |> Async.StartAsTask :> Task
--- SourceWrappers/Stash.fs
+++ SourceWrappers/Stash.fs
@@ -6,32 +6,26 @@ open System.Threading.Tasks
 open DeviantArtFs
 open FSharp.Control
 
-type StashPostWrapper(entry: DeviantArtFs.Stash.DeltaResponse.Entry) =
+type StashPostWrapper(itemId: int64, metadata: DeviantArtFs.Stash.DeltaResponse.Metadata, token: IDeviantArtAccessToken) =
     let imageUrl =
-        entry.Metadata.Files
+        metadata.Files
         |> Seq.sortByDescending (fun f -> f.Width)
         |> Seq.map (fun f -> f.Src)
         |> Seq.tryHead
         |> Option.defaultValue null
 
-    let delete = async {
-        return raise (new NotImplementedException())
-        //let req = new DeviantartApi.Requests.Stash.DeleteRequest(entry.ItemId)
-        //do! Swu.executeAsync req |> Swu.whenDone ignore
-    }
-
-    member __.ItemId = entry.Itemid
+    member __.ItemId = itemId
 
     interface IRemotePhotoPost with
-        member this.Title = entry.Metadata.Title
-        member this.HTMLDescription = entry.Metadata.ArtistComments |> Option.defaultValue ""
+        member this.Title = metadata.Title
+        member this.HTMLDescription = metadata.ArtistComments |> Option.defaultValue ""
         member this.Mature = false
         member this.Adult = false
-        member this.Tags = entry.Metadata.Tags :> seq<string>
-        member this.Timestamp = entry.Metadata.CreationTime |> Option.map Swu.fromUnixTime |> Option.defaultValue DateTime.MinValue
+        member this.Tags = metadata.Tags :> seq<string>
+        member this.Timestamp = metadata.CreationTime |> Option.map Swu.fromUnixTime |> Option.defaultValue DateTime.MinValue
         member this.ViewURL =
             let url = new StringBuilder()
-            let mutable itemId = entry.Itemid |> Option.defaultValue 0L
+            let mutable itemId = itemId
             while itemId > int64 0 do
                 let n = itemId % int64 36
                 let c = "0123456789abcdefghijklmnopqrstuvwxyz".[int32 n]
@@ -41,13 +35,13 @@ type StashPostWrapper(entry: DeviantArtFs.Stash.DeltaResponse.Entry) =
             url.ToString()
         member this.ImageURL = imageUrl
         member this.ThumbnailURL =
-            entry.Metadata.Thumb
+            metadata.Thumb
             |> Option.map (fun t -> t.Src)
             |> Option.defaultValue imageUrl
 
     interface IDeletable with
         member this.SiteName = "Sta.sh"
-        member this.DeleteAsync() = delete |> Async.StartAsTask :> Task
+        member this.DeleteAsync() = DeviantArtFs.Stash.Delete.ExecuteAsync token itemId
 
 type UnorderedStashSourceWrapper(token: IDeviantArtAccessToken) =
     inherit AsyncSeqWrapper()
@@ -67,7 +61,7 @@ type UnorderedStashSourceWrapper(token: IDeviantArtAccessToken) =
                 result.Entries
                 |> Seq.filter (fun e -> (not << Option.isNone) e.Itemid)
                 |> Seq.filter (fun e -> (not << isNull) e.Metadata.Files)
-                |> Seq.map StashPostWrapper
+                |> Seq.map (fun e -> StashPostWrapper(e.Itemid.Value, e.Metadata, token))
             for o in wrappers do
                 yield o :> IPostBase
 
