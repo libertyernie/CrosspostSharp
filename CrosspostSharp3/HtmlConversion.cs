9ebe4071a44873de484d66620ef0e7fbde3a746f
libertyernie <maybeway36@gmail.com>
2019-04-25 19:39:06 -0500

    If the text of a link is part of the URL, replace with the whole URL when converting to text

--- CrosspostSharp3/HtmlConversion.cs
+++ CrosspostSharp3/HtmlConversion.cs
@@ -14,6 +14,16 @@ namespace CrosspostSharp3 {
 			using (var w = new System.Windows.Forms.WebBrowser()) {
 				w.Navigate("about:blank");
 				w.Document.Write(html);
+
+				var coll = w.Document.GetElementsByTagName("a");
+				for (int i = 0; i < coll.Count; i++) {
+					var a = coll[i];
+					string href = a.GetAttribute("href");
+					if (href.Contains(a.InnerText.Replace("…", ""))) {
+						a.InnerText = href;
+					}
+				}
+
 				return w.Document.Body.InnerText?.Trim() ?? "";
 			}
 		}
