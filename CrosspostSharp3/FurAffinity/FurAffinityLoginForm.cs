81700b3bc0ce6888f0597c1dd0de96a8cc71c00b
Isaac Schemm <isaacschemm@gmail.com>
2020-12-10 15:09:41 -0600

    Remove FurAffinity login form and replace with dialog asking for cookies directly

---
+++ CrosspostSharp3/FurAffinity/FurAffinityLoginForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrosspostSharp3 {
+	public partial class FurAffinityLoginForm : Form {
+		public string ACookie => textBox1.Text;
+		public string BCookie => textBox2.Text;
+
+		public FurAffinityLoginForm() {
+			InitializeComponent();
+		}
+	}
+}
