fb197d13aed32a267db6dbd8348b85387f9798db
Isaac Schemm <isaacschemm@gmail.com>
2020-12-31 12:59:43 -0600

    Remove PagedConsumer

--- SourceWrappers/AsyncSeqWrapper.fs
+++ SourceWrappers/AsyncSeqWrapper.fs
@@ -24,50 +24,15 @@ type AsyncSeqWrapperUserInfo = {
         member this.IconUrl = this.icon_url |> Option.defaultValue "https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif"
 
 [<AbstractClass>]
-type AsyncSeqWrapper() as this =
-    let cache = lazy (
-        printfn "%s: Initializing cache" this.Name
-        this.FetchSubmissionsInternal() |> AsyncSeq.cache
-    )
-
-    let user = lazy (this.FetchUserInternal() |> Async.StartAsTask)
-
+type AsyncSeqWrapper() =
     abstract member Name: string with get
     abstract member FetchUserInternal: unit -> Async<AsyncSeqWrapperUserInfo>
     abstract member FetchSubmissionsInternal: unit -> AsyncSeq<IPostBase>
 
-    member __.GetSubmissions() = cache.Force()
-    member __.GetUserAsync() = user.Force()
-
-    member __.WhoamiAsync() =
-        this.GetUserAsync()
-        |> Async.AwaitTask
-        |> Swu.whenDone (fun u -> u.username)
-        |> Async.StartAsTask
-    member __.GetUserIconAsync() =
-        this.GetUserAsync()
-        |> Async.AwaitTask
-        |> Swu.whenDone (fun u -> u.icon_url)
-        |> Swu.whenDone (Option.defaultValue "https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif")
-        |> Async.StartAsTask
-
-    interface AsyncSeq<IPostBase> with
-        member __.GetEnumerator() = cache.Value.GetEnumerator()
     interface IArtworkSource with
         member this.Name = this.Name
         member this.GetPostsAsync() = this.FetchSubmissionsInternal() |> AsyncSeq.toAsyncEnum
         member this.GetUserAsync() = this.FetchUserInternal() |> Swu.whenDone (fun a -> a :> IAuthor) |> Async.StartAsTask
-
-type OrderedAsyncSeqWrapper(wrapper: AsyncSeqWrapper) =
-    inherit AsyncSeqWrapper()
-
-    override __.Name = wrapper.Name
-    override __.FetchSubmissionsInternal() = asyncSeq {
-        let! all = wrapper.GetSubmissions() |> AsyncSeq.toListAsync
-        for x in all |> Seq.sortByDescending (fun x -> x.Timestamp) do
-            yield x
-    }
-    override __.FetchUserInternal() = wrapper.FetchUserInternal()
 
 type AsyncSeqWrapperOfSeq(name: string, seq: seq<IPostBase>) =
     inherit AsyncSeqWrapper()
